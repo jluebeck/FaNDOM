fd3c9dc480096a270be8ac2b2d002efd18461c80
siavashre <siavash.raisi@gmail.com>
2020-06-26 19:14:37 -0700

    autorescale documented

--- autorescale.py
+++ autorescale.py
@@ -1,3 +1,8 @@
+import os
+import argparse
+import random
+
+# Function for parsing cmap files
 def parse_cmap(cmapf, keep_length=False):
     cmaps = {}
     # contigCovs = {}
@@ -22,7 +27,7 @@ def parse_cmap(cmapf, keep_length=False):
                     cmaps[fD["CMapId"]][int(fD["SiteID"])] = float(fD["Position"])
     return cmaps
 
-
+#function for parsing bnx files
 def parse_bnx(bnxF, keep_length=False):
     moleculeD = {}
     with open(bnxF) as infile:
@@ -39,20 +44,28 @@ def parse_bnx(bnxF, keep_length=False):
 
     return moleculeD
 
-
-import argparse
-import random
-
+# size of downsample for detecting ratio
 sampleSize = 500
 
 parser = argparse.ArgumentParser()
+# Query Dir
 parser.add_argument("-q", "--query", help="query directory", required=True)
+# Ref Dir
 parser.add_argument("-r", "--ref", help="reference directory", required=True)
+# Path to folder of FaNDOM
 parser.add_argument("-f", "--fandom", help="FaNDOM directory", required=True)
+# Output Dir
 parser.add_argument("-o", "--output", help="Output directory", required=True)
-parser.add_argument("-t", "--thread", help="Output directory", required=True)
+# Number of thread
+parser.add_argument("-t", "--thread", help="Number of Thread", required=True)
+# Sample size
+parser.add_argument("-s", "--samplesize", help="Sample size for detecting rescale factor", required=False)
 args = parser.parse_args()
 
+if len(args.samplesize) > 0:
+    sampleSize= int(args.samplesize)
+
+#Only parse cmap or bnx files
 if args.query.endswith('.bnx'):
     query = parse_bnx(args.query,True)
 elif args.query.endswith('.cmap'):
@@ -60,6 +73,7 @@ elif args.query.endswith('.cmap'):
 else:
     print('Query file is not in cmap or bnx format')
     return
+
 if args.fandom.endswith('/'):
     fandom = args.fandom
 else:
@@ -72,11 +86,17 @@ if args.output.endswith('/'):
     output = args.output[:-1]
 else:
     output = args.output
+
+#Down sample molecules
 selected = [list(query.keys())[i] for i in random.sample(range(0, len(query)), sampleSize)]
-import os
+
+#contail scaled moleculs
 scaledSample = {}
+#contain result and their score
 result = {}
-for i in range(0, 11):
+
+for i in range(-4, 11):
+    #write new resclaed sample on file
     with open(output+'/a.cmap','w') as f :
         for k in selected:
             scaledSample[k] = query[k].copy()
@@ -92,9 +112,10 @@ for i in range(0, 11):
                         scaledSample[k][index]) + '\t0.0\t1.0\t1.0\t17.0000\t0.0000\n')
                 else:
                     f.write(k + '\t'+str(length)+'\t'+str(number_label)+'\t'+str(index)+'\t1\t'+str(scaledSample[k][index])+'\t0.0\t1.0\t1.0\t17.0000\t0.0000\n')
-
+    #Run Fandom on new resclaed molecules
     os.system(fandom+'./FaNDOM'+' -t='+args.thread+' -r='+args.ref+' -q='+output+'/a.cmap -sname='+output+'/test '+ '-outfmt=xmap')
     sum_score = 0
+    #Sum scores of molecule
     with open(output+'/test.xmap','r') as f:
         for line in f:
             if not line.startswith('#'):
@@ -106,6 +127,8 @@ os.system('rm '+output+'/a.cmap')
 max_index = int(max(result,key=result.get))
 
 print('Max score with rescale factor: ', max_index)
+
+#Rescla all molecules with detected rescale factor
 if args.query.endswith('.bnx'):
     with open(output+'/rescaled.bnx','w') as f:
         with open(args.query,'r') as g:
