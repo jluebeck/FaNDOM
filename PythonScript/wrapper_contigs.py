7e290660f69a9a6ec1e3d6ccfe0df403e3a0f0fe
siavashre <siavash.raisi@gmail.com>
2021-02-25 19:35:46 -0800

    Update wrapper_contigs.py

--- PythonScript/wrapper_contigs.py
+++ PythonScript/wrapper_contigs.py
@@ -45,31 +45,20 @@ if p == 1:
 	os.system(assemble_cmd)
 	os.chdir(out)
 	os.system(
-	    'cat ' + out + args.name + '_full_post_process_assembled.xmap ' + out + name + '_partial_filtered_post_process.xmap >' + name + '_all.xmap')
+	    'cat ' + args.name + '_full_post_process_assembled.xmap ' + name + '_partial_filtered_post_process.xmap >' + name + '_all.xmap')
 	os.chdir( args.fandom)
 	print("assemble reads2")
 	assemble_cmd = 'python PythonScript/assemble_reads.py -i ' + out + name + '_all.xmap' + ' -o ' + out + name + '_all'
 	os.system(assemble_cmd)
-	filter_partial_cmd = 'python PythonScript/filter_contig2.py -i ' + out + name + '_all_assembled.xmap ' + ' -o ' + out + name + '_all_assembled2'
+	filter_partial_cmd = 'python PythonScript/filter_contig2.py -i ' + out + name + '_all_assembled.xmap ' + ' -o ' + out + name + '_final_alignment'
 	os.system(filter_partial_cmd)
 	print("SV detect")
-	sv_detect_cmd = 'python3 PythonScript/SV_detection_contigs.py -i ' + out + name + '_all_assembled2.xmap -l 1 -c ' + c + ' -r=' + args.ref+ ' -q ' + args.query +' -g '+gene_dir+ ' -o ' +out + 'SV.txt'
+	sv_detect_cmd = 'python3 PythonScript/SV_detection_contigs.py -i ' + out + name + '_final_alignment.xmap -l 1 -c ' + c + ' -r=' + args.ref+ ' -q ' + args.query +' -g '+gene_dir+ ' -o ' +out + 'SV.txt'
 	os.system(sv_detect_cmd)
-	os.chdir( out)
-	# os.system('cat '+ out + args.name + '_full_post_process.xmap '+ out + name + '_partial_filtered_post_process.xmap >' + name + '_all_indel.xmap')
-	os.chdir( args.fandom)
 	print("Indel detect")
-	indel_detect_cmd = 'python3 PythonScript/indel_detection_contigs.py -r ' + args.ref +' -g '+gene_dir+ ' -c ' + c + ' -m ' + args.query + ' -a ' + out + name + '_all_assembled2.xmap -o ' + out + 'indel.txt'
+	indel_detect_cmd = 'python3 PythonScript/indel_detection_contigs.py -r ' + args.ref +' -g '+gene_dir+ ' -c ' + c + ' -m ' + args.query + ' -a ' + out + name + '_final_alignment.xmap -o ' + out + 'indel.txt'
 	os.system(indel_detect_cmd)
-# 	print('Done')
-# 	os.chdir( out)
-# 	os.system("grep 'ins' indel > insertion")
-# 	os.system("grep 'del' indel > deletion")
-	# os.chdir( args.fandom)
-	# cluser_cmd = 'python PythonScript/cluster_indels.py -i ' + out +'insertion '+ '-o '+out+'insertion_clustered'
-	# os.system(cluser_cmd)
-	# cluser_cmd = 'python PythonScript/cluster_indels.py -i ' + out +'deletion '+ '-o '+out+'deletion_clustered'
-	# os.system(cluser_cmd)
+	print('Done')
 if p==2:
 	fandom_cmd = './FaNDOM -t=' + args.thread + ' -r=' + args.ref + ' -q=' + args.query + ' -sname=' + out + args.name + ' -outfmt=xmap '
 	os.system(fandom_cmd)
@@ -77,30 +66,12 @@ if p==2:
 	os.system(remove_part_cmd)
 	filter_partial_cmd = 'python PythonScript/filter_contigs.py -i ' + out + args.name + '_partial.xmap -r ' + args.ref + ' -o ' + out + name + '_partial_filtered'
 	os.system(filter_partial_cmd)
-	# filter_partial_post_cmd = 'python PythonScript/post_process.py -f ' + out + name + '_partial_filtered.xmap -d ' + out + args.name + '_preprocess_dic -o ' + out + name + '_partial_filtered'
-	# os.system(filter_partial_post_cmd)
-	# full_post_cmd = 'python PythonScript/post_process.py -f ' + out + args.name + '_full.xmap -d ' + out + args.name + '_preprocess_dic -o ' + out + args.name + '_full'
-	# os.system(full_post_cmd)
-	# assemble_cmd = 'python PythonScript/assemble_reads.py -i ' + out + args.name + '_full_post_process.xmap -o ' + out + args.name + '_full_post_process'
-	# os.system(assemble_cmd)
 	os.chdir(out)
 	os.system(
-	    'cat ' + out + args.name + '_full.xmap ' + out + name + '_partial_filtered.xmap >' + name + '_all.xmap')
+	    'cat '  + args.name + '_full.xmap ' + name + '_partial_filtered.xmap >' + name + '_final_alignment.xmap')
 	os.chdir( args.fandom)
-	# assemble_cmd = 'python PythonScript/assemble_reads.py -i ' + out + name + '_all.xmap' + ' -o ' + out + name + '_all'
-	# os.system(assemble_cmd)
-	sv_detect_cmd = 'python3 PythonScript/SV_detection_contigs.py -i ' + out + name + '_all.xmap -l 1 -c ' + c + ' -r ' + args.ref+ ' -q ' + args.query +' -g '+gene_dir+ ' -o ' +out + 'SV.txt'
+	sv_detect_cmd = 'python3 PythonScript/SV_detection_contigs.py -i ' + out + name + '_final_alignment.xmap -l 1 -c ' + c + ' -r ' + args.ref+ ' -q ' + args.query +' -g '+gene_dir+ ' -o ' +out + 'SV.txt'
 	os.system(sv_detect_cmd)
-	os.chdir( out)
-	# os.system('cat '+ out + args.name + '_full_post_process.xmap '+ out + name + '_partial_filtered_post_process.xmap >' + name + '_all_indel.xmap')
-	os.chdir( args.fandom)
-	indel_detect_cmd = 'python3 PythonScript/indel_detection_contigs.py -r ' + args.ref + ' -g '+gene_dir+' -c ' + c + ' -m ' + args.query + ' -a ' + out + name + '_all.xmap -o ' + out + 'indel.txt'
+	indel_detect_cmd = 'python3 PythonScript/indel_detection_contigs.py -r ' + args.ref + ' -g '+gene_dir+' -c ' + c + ' -m ' + args.query + ' -a ' + out + name + '_final_alignment.xmap -o ' + out + 'indel.txt'
 	os.system(indel_detect_cmd)
-# 	os.chdir( out)
-# 	os.system("grep 'ins' indel > insertion")
-# 	os.system("grep 'del' indel > deletion")
-# os.chdir( args.fandom)
-# cluser_cmd = 'python PythonScript/cluster_indels.py -i ' + out +'insertion '+ '-o '+out+'insertion_clustered'
-# os.system(cluser_cmd)
-# cluser_cmd = 'python PythonScript/cluster_indels.py -i ' + out +'deletion '+ '-o '+out+'deletion_clustered'
-# os.system(cluser_cmd)
+	print('Done')
