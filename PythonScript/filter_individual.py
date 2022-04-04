0b0e8d0c1f445e8397214a18b5e6a4328b5cfc3c
siavashre <siavash.raisi@gmail.com>
2022-04-04 02:55:33 -0700

    Update filter_individual.py
    remove comments

--- PythonScript/filter_individual.py
+++ PythonScript/filter_individual.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-# from scipy.stats import entropy
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -26,38 +25,10 @@ def span_alignment(a1,a2,a3,a4):
         if nov_overlap(a1,a2,a3,a4) >= minimum_nonoverlap_length:
             return 1
     return 0 
-
-# d = {}
-# d = defaultdict(lambda: [], d)
-# perv = 0
-# with open(args.ref, 'r') as f:
-#     for line in f:
-#         if not line.startswith('#'):
-#             line = line.strip().split('\t')
-#             id = int(line[0])
-#             pos = float(line[5])
-#             d[id].append(pos)
-# low_complexity ={}
-# low_complexity = defaultdict(lambda: [], low_complexity)
-# for k in d.keys():
-#     distances = d[k]
-#     for i in range(0, len(distances) - 6):
-#         dis = distances[i:i + 6]
-#         b = []
-#         for j in range(0, len(dis) - 1):
-#             b.append(dis[j + 1] - dis[j])
-#         a = entropy(b, base = 5)
-#         if abs(a - 1) < 0.005:
-#             for j in range(0, len(dis) - 1):
-#                 low_complexity[k].append(i+j+1)
-#             low_complexity[k].append(i + 6)
-# for k in low_complexity.keys():
-#     low_complexity[k] = sorted(list(set(low_complexity[k])))
 d = {}
 # kk = 1;
 d = defaultdict(lambda: [], d)
 with open(args.output + '.xmap', 'w') as g:
-    print('salam')
     with open(args.input, 'r') as f:
         for line in f:
             if not line.startswith('#'):
@@ -67,20 +38,6 @@ with open(args.output + '.xmap', 'w') as g:
                 a1 = float(line2[3])
                 a2 = float(line2[4])
                 align = line2[13]
-                # a = align.split((')('))
-                # a[0] = a[0][1:]
-                # count = 0
-                # for i in a:
-                #     r = i.split(',')[0]
-                #     r = int(r)
-                #     if r in low_complexity[int(line2[2])]:
-                #         count += 1
-                #     else:
-                #         count = count - 0.2
-                # if count > 5:
-                #     continue
-                # b1 = int(align.split(')')[0].split(',')[1])
-                # b2 = int(align.split(')')[-2].split(',')[1])
                 align = align.split(',')
                 separate_lines = len(align) - 1
                 score = conf / separate_lines
@@ -103,10 +60,6 @@ with open(args.output + '.xmap', 'w') as g:
             for j in range(i + 1, len(d[k])):
                 b = d[k][j]
                 b1 = b[1].split('\t')
-                # if min(float(a1[3]), float(a1[4])) - 10000 <= min(float(b1[3]), float(b1[4])) and max(float(a1[3]),
-                #                                                                                      float(a1[
-                #                                                                                                4])) + 10000 >= max(
-                #     float(b1[3]), float(b1[4])):
                 if span_alignment(float(a1[3]), float(a1[4]), float(b1[3]), float(b1[4])) >=0.8:
                     deleted.append(b)
                 elif min(float(b1[3]), float(b1[4])) - 2000 <= min(float(a1[3]), float(a1[4])) and max(float(b1[3]),
